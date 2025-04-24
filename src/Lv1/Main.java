87bd00c2a468f43d157dfc99b6878fe0ac4da4cd
Hyeon woo <renzpark@naver.com>
2025-04-24 20:59:19 +0900

    1단계 and 2단계 90퍼 완성

--- src/Lv1/Main.java
+++ src/Lv1/Main.java
@@ -13,7 +13,7 @@ public class Main {
             System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
             System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
             System.out.println("종료      | 종료");
-            System.out.println("<- // 0을 입력");
+            System.out.print("0을 입력시 종료: ");
 
             int num = scanner.nextInt();
 
--- src/Lv2/MenuItem.java
+++ src/Lv2/MenuItem.java
@@ -15,23 +15,8 @@ public class MenuItem {
         this.price = price;
         this.explain = explain;
     }
-
-
-public static void main(String[] args) {
-    List<MenuItem> menuItems = new ArrayList<>();
-
-    menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-    menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-    menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-    menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
-
-    Scanner scanner = new Scanner(System.in);
-
-    for (int i = 0; i < menuItems.size(); i++) {
-        MenuItem item = menuItems.get(i);
-        System.out.printf("%d. %s | W %.1f | %s", i + 1, item.name, item.price, item.explain);
-    }
 }
-}
+
+
 
 
