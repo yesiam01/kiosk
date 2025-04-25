19d9356abf72220881af501d8ee554e55bc86699
Hyeon woo <renzpark@naver.com>
2025-04-25 17:24:10 +0900

    1단계 and 2단계 완성 3단계 구현중

--- src/Lv3/Kiosk.java
+++ src/Lv3/Kiosk.java
@@ -1,19 +1,21 @@
-package Lv2;
+package Lv3;
 
+import Lv2.MenuItem;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        List<MenuItem> menuItems = new ArrayList<>();
+public class Kiosk {
 
-        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+    ArrayList<MenuItem> menuItems = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+    menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+    menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+    menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+    menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+    public void start() {
+
         while (true) {
             for (int i = 0; i < menuItems.size(); i++) {
                 MenuItem item = menuItems.get(i);
---
+++ src/Lv3/Main.java
@@ -0,0 +1,15 @@
+package Lv3;
+
+import Lv2.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<MenuItem> menuItems = new ArrayList<>();
+        Kiosk kiosk = new Kiosk();
+
+
+    }
+}
