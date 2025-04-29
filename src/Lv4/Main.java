181d89cac2b7fcdb5535790b3bb808934d86e7b7
Hyeon woo <renzpark@naver.com>
2025-04-29 13:19:22 +0900

    1단계 완성 2단계 완성 3단계 완성 4단계 완성

--- src/Lv4/Main.java
+++ src/Lv4/Main.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<MenuItem> menuItems = new ArrayList<>();
-        Kiosk kiosk = new Kiosk(menuItems);
         Menu menu = new Menu();
 
+        menu.addMenuItem(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menu.addMenuItem(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menu.addMenuItem(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menu.addMenuItem(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        Kiosk kiosk = new Kiosk(menu.getMenuItems());
+        kiosk.start();
     }
 }
---
+++ src/Lv5/Main.java
@@ -0,0 +1,4 @@
+package Lv5;
+
+public class Main {
+}
